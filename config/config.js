e9ce9b3808c399422b8956c14c23c8c81b2371a4
charisTheo <dev@charistheo.io>
2020-04-01 19:56:40 +0100

    Configure dotenv only in dev environment

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
-require('dotenv').config();
-
+const dotenv = require('dotenv');
 const { getServerIpAddress } = require('./../util/serverUtil');
+
+if (process.env.NODE_ENV === 'dev') {
+    dotenv.config();
+}
 
 process.env['SERVER_IP_ADDRESS'] = getServerIpAddress();
